09782fc8885bb32b3744abe8708d075e08996251
DESKTOP-84LKP47\Admin <sotero.alcantara22@gmail.com>
2024-02-10 01:46:08 +0800

    added the if statement for winner and status for the next player

--- tictac-project/src/App.jsx
+++ tictac-project/src/App.jsx
@@ -14,6 +14,9 @@ function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (calWinner(squares) || squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xiSNext) {
       nextSquares[i] = "X";
@@ -23,8 +26,18 @@ function App() {
     setSquares(nextSquares);
     setXIsNext(!xiSNext);
   }
+
+  const winner = calWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xiSNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -42,6 +55,25 @@ function App() {
       </div>
     </>
   );
+}
+function calWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default App;
